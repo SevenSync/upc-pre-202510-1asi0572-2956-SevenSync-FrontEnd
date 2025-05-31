4b748f6fa65680621f0d0ab0e5df2fb46f6b1f77
Fasz0711 <fabrizio0711@hotmail.com>
2025-05-30 20:08:49 -0500

    feat: backend integration with login and register components and add password recovery functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import {PasswordRecoveryComponent} from './components/password-recovery/password-recovery.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  {path: 'password-recovery',component: PasswordRecoveryComponent},
 
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 
