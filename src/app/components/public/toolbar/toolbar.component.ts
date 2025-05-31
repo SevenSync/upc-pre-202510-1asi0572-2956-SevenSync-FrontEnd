289168bbe9a2eac43bad1c6967a41ecc0dab0dce
DevLuiggi <luiggipdeveloper@gmail.com>
2025-05-30 19:17:58 -0500

    feat(toolbar): add links to redirection in toolbar

--- src/app/components/public/toolbar/toolbar.component.ts
+++ src/app/components/public/toolbar/toolbar.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from '@angular/material/icon';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
-    imports: [
-      MatToolbarModule, MatButtonModule, MatIconModule
-    ],
+  imports: [
+    MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, RouterLinkActive
+  ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
