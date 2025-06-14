f4c2b89871ec63497d19c31ac217b6e84228eaf6
Lucio Yen <U202213143@upc.edu.pe>
2025-06-14 05:47:03 -0500

    feat: add environment variables for API credentials

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  perenualApiKey: 'TU_API_KEY_PRODUCCIÓN',
+  perenualBaseUrl: 'https://perenual.com/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  perenualApiKey: 'sk-JmBk684d2d9dc994011009',
+  perenualBaseUrl: 'https://perenual.com/api'
+};
