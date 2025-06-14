27e39cae29a5733e0e9541e191e30c23bda97f58
DevLuiggi <luiggipdeveloper@gmail.com>
2025-06-13 22:09:28 -0500

    refactor(edit-pot): add general styles for mat-form

--- src/app/components/edit-pot/edit-pot.component.ts
+++ src/app/components/edit-pot/edit-pot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit, ViewEncapsulation } from "@angular/core"
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms"
 import { ActivatedRoute, Router } from "@angular/router"
 import { MatButtonModule } from "@angular/material/button"
@@ -37,6 +37,7 @@ interface PotEditData {
   ],
   templateUrl: "./edit-pot.component.html",
   styleUrl: "./edit-pot.component.css",
+  encapsulation: ViewEncapsulation.None
 })
 export class EditPotComponent implements OnInit {
   editForm: FormGroup
