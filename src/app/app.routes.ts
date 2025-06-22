242c92892b64fd2d47e8f6110aa54b3952d1dda8
Fasz0711 <fabrizio0711@hotmail.com>
2025-06-22 15:42:05 -0500

    feat(membership): add payment-done component and enhance membership comparison section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import {PaymentComponent} from './components/payment/payment.component';
 import {NotificationsComponent} from './components/notifications/notifications.component';
 import {PotsComponent} from './components/pots/pots.component';
 import {CreateProfileComponent} from './profile_and_preferences/presentation/create-profile/create-profile.component';
+import {PaymentDoneComponent} from './components/payment-done/payment-done.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -20,6 +21,7 @@ export const routes: Routes = [
   { path:'payment', component: PaymentComponent },
   { path: 'notifications', component: NotificationsComponent },
   { path: "pots", component: PotsComponent },
+  { path: "payment-done", component: PaymentDoneComponent },
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
 
 ];
